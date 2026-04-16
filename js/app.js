8da7e097d57b1eee777ada6e22619bcd2c00fc41
JanekPrz <jasio.p@icloud.com>
2026-04-16 13:17:06 +0200

    Added timer

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 // Click a button or object to earn points so that I can increase my score.
 // See my current score during the game so that I know how well I am doing.
-// See a countdown timer so that I know how much time is left.
+
+// See a countdown timer so that I know how much time is left. setInterval()
 
 // Variables
 let score = 0;
-
+let time = 60;
 // HMTL DOM
 const button1 = document.getElementById('button1');
 const scoreDisplay = document.getElementById('scoreDisplay');
-
+const timeDisplay = document.getElementById('timeDisplay');
 // UI Functions
 button1.addEventListener('click', () => {
   increaseScore()
@@ -19,3 +20,19 @@ function increaseScore() {
   score++;
   scoreDisplay.innerText = score;
 }
+
+
+function startTimer() {
+  const timer = setInterval(() => {
+    time--;
+    timeDisplay.innerText = "Time left: " + time;
+
+    if (time <= 0) {
+      clearInterval(timer);
+      timeDisplay.innerText = "Game Over";
+    }
+  }, 1000);
+}
+
+// Function calls
+startTimer()
